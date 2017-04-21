31d39a143f09261dffd2dc0b9d70a29602b8a1e9
HarryWu00 <8hwu@stpatrick.org>
2017-04-21 10:51:01 -0500

    Add files via upload

--- Hover.js
+++ Hover.js
@@ -1,3 +1,9 @@
-(function(){
+function(){
   const doc = document;
-})
+  let div = doc.queryselector("div");
+  div.AddEventListener("click",function(){
+    if(div.className=="banner"){
+      div.style.border = solid green;
+    }
+  })
+}
